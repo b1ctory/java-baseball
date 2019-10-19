c2b3a59bc7741081a128c845b1f6ca8cabccd267
b1ctory <suebin710@gmail.com>
2019-10-19 17:23:27 +0900

    feat : create Baseball Model

---
+++ src/main/java/model/BaseBall.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class BaseBall {
+    private final static int MAX_NUMBER = 10;
+    private final static int NUMBER_LENGTH = 3;
+    private static Random RANDOM = new Random();
+    public static HashSet<Integer> randomNumbers;
+
+    public static HashSet<Integer> generateRandomNumber (){
+        randomNumbers = new HashSet<>();
+        while(randomNumbers.size() != NUMBER_LENGTH){
+            randomNumbers.add(RANDOM.nextInt(MAX_NUMBER));
+            randomNumbers.remove(0);
+        }
+        return randomNumbers;
+    }
+}
