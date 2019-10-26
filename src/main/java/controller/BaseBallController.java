57964490060e4c2e0369676a7dd639ed5904cdc3
b1ctory <suebin710@gmail.com>
2019-10-26 19:17:37 +0900

    feat : model 수정

--- src/main/java/controller/BaseBallController.java
+++ src/main/java/controller/BaseBallController.java
@@ -1,78 +1,60 @@
 package controller;
 
 import model.BaseBall;
+import util.RandomGenerator;
 import view.InputView;
 import view.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
+
+import static model.BaseBall.countNothing;
 
 public class BaseBallController {
-    private List<Integer> computerAnswer;
-    private List<Integer> userAnswer;
-    private OutputView outputView = new OutputView();
-    private boolean gameEnd = true;
-
     public static final int REGAME = 1;
+    public static final int ALL_STRIKE_NUM = 3;
+    public static final int NOTHING = 3;
 
-    public BaseBallController(){
-        run();
+    private List<Integer> computerAnswer;
+    private boolean gameContinue = true;
+
+    public void run() {
+        do {
+            computerAnswer = new ArrayList<>(RandomGenerator.generateRandomNumber());
+            startGame();
+            gameContinue = true;
+        } while (Integer.parseInt(InputView.inputRestart()) == REGAME);
     }
 
-    public List<Integer> convertToList(List<String> stringList){
-        List <Integer> integerList = new ArrayList<>();
+    public void startGame() {
+        List<Integer> userAnswer;
+        while (gameContinue) {
+            userAnswer = convertToList(InputView.inputNumber());
+            isAnswer(BaseBall.countStrike(computerAnswer, userAnswer),userAnswer);
+        }
+    }
+
+    public static List<Integer> convertToList(List<String> stringList) {
+        List<Integer> integerList = new ArrayList<>();
         for (int i = 0; i < stringList.size(); i++) {
             integerList.add(Integer.parseInt(stringList.get(i)));
         }
         return integerList;
     }
 
-    private int isStrike(List<Integer> computer , List<Integer> user){
-        return  IntStream.range(0,computer.size())
-                .map(i -> (user.get(i) == computer.get(i) ? 1 : 0))
-                .sum();
-    }
-
-    private int isBall(List<Integer> computer , List<Integer> user){
-        return IntStream.range(0,computer.size())
-                .filter(i -> user.get(i) != computer.get(i))
-                .map(i -> ((user.get(i) == computer.get((i + 1) % 3))
-                        || (user.get(i) == computer.get((i + 2) % 3)))? 1 : 0 )
-                .sum();
-    }
-
-    private void isNothing(List<Integer> computer, List<Integer> user){
-        int nothing = IntStream.range(0,computer.size())
-                .filter(i -> isStrike(computer,user) == 0 && isBall(computer,user) == 0)
-                .sum();
-        if(nothing == 3){
-            outputView.printNothing();
+    public void checkNothing(List<Integer> userAnswer) {
+        countNothing(computerAnswer, userAnswer);
+        if (BaseBall.countNothing(computerAnswer,userAnswer) == NOTHING) {
+            OutputView.printNothing();
         }
     }
 
-    public void isAnswer(int strike) {
-        outputView.printStrike(isStrike(computerAnswer,userAnswer));
-        outputView.printBall(isBall(computerAnswer,userAnswer));
-        isNothing(computerAnswer,userAnswer);
-        if (strike == 3){
-            gameEnd = false;
+    public void isAnswer(int strike, List<Integer> userAnswer) {
+        OutputView.printStrike(BaseBall.countStrike(computerAnswer,userAnswer));
+        OutputView.printBall(BaseBall.countBall(computerAnswer, userAnswer));
+        checkNothing(userAnswer);
+        if (strike == ALL_STRIKE_NUM) {
+            gameContinue = false;
         }
     }
-
-    public void startGame(){
-        while(gameEnd){
-            userAnswer = convertToList(InputView.inputNumber());
-            isAnswer(isStrike(computerAnswer,userAnswer));
-        }
-    }
-
-    public void run(){
-        do{
-            computerAnswer = new ArrayList<>(BaseBall.generateRandomNumber());
-            startGame();
-            gameEnd = true;
-        } while (Integer.parseInt(InputView.inputRestart()) == REGAME);
-    }
-
 }
