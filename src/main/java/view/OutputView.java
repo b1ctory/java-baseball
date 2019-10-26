e81f5be1e8e4753d14fe132deedcd085bf188613
b1ctory <suebin710@gmail.com>
2019-10-26 19:19:55 +0900

    feat : 메소드들 static으로 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,20 +1,19 @@
 package view;
 
 public class OutputView {
-
-    public void printStrike(int strike){
+    public static void printStrike(int strike) {
         if(strike > 0) {
             System.out.println(strike + "스트라이크");
         }
     }
 
-    public void printBall(int ball){
+    public static void printBall(int ball) {
         if(ball > 0) {
             System.out.println(ball + "볼");
         }
     }
 
-    public void printNothing(){
+    public static void printNothing() {
         System.out.println("낫싱");
     }
 }
