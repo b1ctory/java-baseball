7af463c85305ced4abef1be67720706d3cf877d6
b1ctory <suebin710@gmail.com>
2019-10-28 13:36:49 +0900

    feat : model 수정

--- src/main/java/model/BaseBall.java
+++ src/main/java/model/BaseBall.java
@@ -1,31 +1,48 @@
 package model;
 
+import util.BaseBallDto;
+import util.RandomGenerator;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class BaseBall {
-     public static int countStrike(List<Integer> computer, List<Integer> user) {
-        return IntStream.range(0, computer.size())
-                .map(i -> (user.get(i) == computer.get(i) ? 1 : 0))
+    private List<Integer> computerAnswer;
+    private List<Integer> userAnswer;
+
+    public BaseBall(){
+        this.computerAnswer = RandomGenerator.generateRandomNumber();
+    }
+
+    public void initUsers(List<Integer> integerList){
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < integerList.size(); i++) {
+            integers.add(integerList.get(i));
+        }
+        this.userAnswer = integers;
+    }
+
+    public int getStrike() {
+        return IntStream.range(0, computerAnswer.size())
+                .map(i -> (userAnswer.get(i) == computerAnswer.get(i) ? 1 : 0))
                 .sum();
     }
 
-    public static int countBall(List<Integer> computer, List<Integer> user) {
-        return IntStream.range(0, computer.size())
-                .map(i -> isBall(computer,user,i) ? 1 : 0)
+    private int getBall() {
+        return IntStream.range(0, computerAnswer.size())
+                .map(i -> isBall(computerAnswer,userAnswer,i) ? 1 : 0)
                 .sum();
     }
 
-    public static int countNothing(List<Integer> computer, List<Integer> user) {
-        return IntStream.range(0, computer.size())
-                .filter(i -> countStrike(computer, user) == 0 && countBall(computer, user) == 0)
-                .sum();
+    public BaseBallDto getResult() {
+        return new BaseBallDto(getStrike(), getBall());
     }
 
-    private static boolean isBall(List<Integer> computer, List<Integer> user, int i) {
-        if((user.get(i) == computer.get((i + 1) % 3))  || (user.get(i) == computer.get((i + 2) % 3)))
+    private boolean isBall(List<Integer> computer, List<Integer> user, int i) {
+        if ((user.get(i) == computer.get((i + 1) % 3))  || (user.get(i) == computer.get((i + 2) % 3))) {
             return true;
+        }
         return false;
     }
-
 }
