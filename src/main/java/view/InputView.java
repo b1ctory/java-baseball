e2f8f42f003d0a0230cf610a6d68c2d2772b0f19
b1ctory <suebin710@gmail.com>
2019-10-28 13:50:35 +0900

    feat : inputView 수정
    1) 사용자의 답을 String List에 저장하여 Integer List로 변환해주는 방식 대신,
    사용자의 답을 String으로 받아 Integer List에 저장해주는 방식으로 수정.
    2) 숫자가 아닌 입력값에 대한 예외처리 추가
    3) inputRestart() 메소드에 대한 입력값과 리턴값을 int형으로 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,19 +3,25 @@ package view;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
-    private static Scanner SCANNER;
-
-    public static List<String> inputNumber() {
-        SCANNER = new Scanner(System.in);
-        System.out.print("숫자를 입력해 주세요 : ");
-        return Arrays.asList(SCANNER.nextLine().split(""));
+    public static List<Integer> inputNumbers() {
+        try {
+            System.out.print("숫자를 입력해 주세요 : ");
+            String numbers = new Scanner(System.in).nextLine();
+            return Arrays.stream(numbers.split(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            System.out.println("숫자만 입력하세요.");
+            return inputNumbers();
+        }
     }
 
-    public static String inputRestart() {
+    public static int inputRestart() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ");
-        return SCANNER.nextLine();
+        return new Scanner(System.in).nextInt();
     }
 }
