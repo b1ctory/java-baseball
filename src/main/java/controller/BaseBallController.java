fb7ccadd3c200c4f6f2b34aaf68389c3965ae40d
b1ctory <suebin710@gmail.com>
2019-10-28 13:34:43 +0900

    feat : controller 수정
    Baseball 볼판정에 대한 로직들을 Model로 옮김

--- src/main/java/controller/BaseBallController.java
+++ src/main/java/controller/BaseBallController.java
@@ -1,60 +1,35 @@
 package controller;
 
 import model.BaseBall;
-import util.RandomGenerator;
 import view.InputView;
 import view.OutputView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static model.BaseBall.countNothing;
-
 public class BaseBallController {
-    public static final int REGAME = 1;
-    public static final int ALL_STRIKE_NUM = 3;
-    public static final int NOTHING = 3;
+    private static final int REGAME = 1;
+    private static final int THREE_STRIKE = 3;
+    private boolean flag = true;
 
-    private List<Integer> computerAnswer;
-    private boolean gameContinue = true;
+    private BaseBall baseBall;
 
     public void run() {
         do {
-            computerAnswer = new ArrayList<>(RandomGenerator.generateRandomNumber());
+            this.baseBall = new BaseBall();
             startGame();
-            gameContinue = true;
-        } while (Integer.parseInt(InputView.inputRestart()) == REGAME);
+            flag = true;
+        } while(InputView.inputRestart() == REGAME);
     }
 
     public void startGame() {
-        List<Integer> userAnswer;
-        while (gameContinue) {
-            userAnswer = convertToList(InputView.inputNumber());
-            isAnswer(BaseBall.countStrike(computerAnswer, userAnswer),userAnswer);
+        while(flag) {
+            baseBall.initUsers(InputView.inputNumbers());
+            OutputView.printStatus(baseBall.getResult());
+            isAnswer(baseBall.getStrike());
         }
     }
 
-    public static List<Integer> convertToList(List<String> stringList) {
-        List<Integer> integerList = new ArrayList<>();
-        for (int i = 0; i < stringList.size(); i++) {
-            integerList.add(Integer.parseInt(stringList.get(i)));
-        }
-        return integerList;
-    }
-
-    public void checkNothing(List<Integer> userAnswer) {
-        countNothing(computerAnswer, userAnswer);
-        if (BaseBall.countNothing(computerAnswer,userAnswer) == NOTHING) {
-            OutputView.printNothing();
-        }
-    }
-
-    public void isAnswer(int strike, List<Integer> userAnswer) {
-        OutputView.printStrike(BaseBall.countStrike(computerAnswer,userAnswer));
-        OutputView.printBall(BaseBall.countBall(computerAnswer, userAnswer));
-        checkNothing(userAnswer);
-        if (strike == ALL_STRIKE_NUM) {
-            gameContinue = false;
+    private void isAnswer(int strike) {
+        if (strike == THREE_STRIKE) {
+            flag = false;
         }
     }
 }
